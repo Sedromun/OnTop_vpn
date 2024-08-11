9e9b76fb02a8b62e4192898137dfa840d959dbf8
sedromun <vanart2004@mail.ru>
2024-08-11 23:01:58 +0300

    ngnix

--- backend.py
+++ backend.py
@@ -7,4 +7,4 @@ from servers.outline_keys import get_key
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host=HOST, port=PORT, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
