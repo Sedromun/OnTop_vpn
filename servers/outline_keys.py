2e571a996d1ef78b7630c403a600e4df83b4269f
sedromun <vanart2004@mail.ru>
2024-08-30 00:34:54 +0300

    fix keys country changes

--- servers/outline_keys.py
+++ servers/outline_keys.py
@@ -1,5 +1,5 @@
 from config import outline_client
-from database.controllers.key import create_key, get_order_country_key
+from database.controllers.key import create_key, get_order_country_key, delete_key
 from database.controllers.order import update_order, get_order
 
 
@@ -7,7 +7,7 @@ def get_key(country: str, order_id: int) -> str:
     order = get_order(order_id)
     key = get_order_country_key(country=country, order=order)
     if key is not None:
-        return key.key
+        delete_key(key.id)
 
     new_key = outline_client[country].create_key().access_url
     create_key({"order_id": order_id, "country": country, "key": new_key})
