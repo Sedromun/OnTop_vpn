0f3dab317a3c41810a47bf930e52cc3724fde569
sedromun <vanart2004@mail.ru>
2024-10-04 04:08:16 +0300

    fix

--- handlers/info.py
+++ handlers/info.py
@@ -239,7 +239,7 @@ async def info_countries_callback(
     orders = user.orders
     on_auto_orders = []
     for order in orders:
-        if order.payment_id is not None:
+        if order.payment_id != '' and order.payment_id is not None:
             on_auto_orders.append(order)
 
     if len(orders) == 0:
