602d1348d9475abaf2d8c79d873d0a43d818c5e3
sedromun <vanart2004@mail.ru>
2024-10-08 21:12:52 +0300

    fix

--- logs/__init__.py
+++ logs/__init__.py
@@ -2,17 +2,24 @@ import datetime
 import logging
 import traceback
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    filename=f".logs/{datetime.datetime.now().strftime('%Y_%m_%d')}_root_log.log"
+)
+
 bot_logger = logging.getLogger("bot")
 checker_logger = logging.getLogger("checker")
 backend_logger = logging.getLogger("backend")
 
-bot_fh = logging.FileHandler(f".logs/{datetime.datetime.now().strftime('%Y_%m_%d_%H_%M')}_bot_log.log")
+bot_fh = logging.FileHandler(f".logs/{datetime.datetime.now().strftime('%Y_%m_%d')}_bot_log.log")
 bot_fh.setLevel(logging.INFO)
 
-checker_fh = logging.FileHandler(f".logs/{datetime.datetime.now().strftime('%Y_%m_%d_%H_%M')}_checker_log.log")
+checker_fh = logging.FileHandler(f".logs/{datetime.datetime.now().strftime('%Y_%m_%d')}_checker_log.log")
 checker_fh.setLevel(logging.INFO)
 
-backend_fh = logging.FileHandler(f".logs/{datetime.datetime.now().strftime('%Y_%m_%d_%H_%M')}_backend_log.log")
+backend_fh = logging.FileHandler(f".logs/{datetime.datetime.now().strftime('%Y_%m_%d')}_backend_log.log")
 backend_fh.setLevel(logging.INFO)
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
