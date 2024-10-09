4a4b611f16b989152146206d5837ed7d900b3ffd
sedromun <vanart2004@mail.ru>
2024-10-09 19:13:32 +0300

    fix

--- text/texts.py
+++ text/texts.py
@@ -71,7 +71,7 @@ def get_pay_text():
 def get_key_data(order):
     return (
         f"Страна: {order.country} {COUNTRIES[order.country]}\n\n"
-        f"Дата истечения ключа" + (" и продления подписки" if (order.payment_id != "" and order.payment_id is not None) else "") + f":{(order.expiration_date.astimezone(datetime.timezone.utc) + datetime.timedelta(hours=3)).strftime(datetime_format)}\n"
+        f"Дата истечения ключа" + (" и продления подписки" if (order.payment_id != "" and order.payment_id is not None) else "") + f": {(order.expiration_date.astimezone(datetime.timezone.utc) + datetime.timedelta(hours=3)).strftime(datetime_format)}\n"
         f"(осталось {get_left_time(order.expiration_date.astimezone(datetime.timezone.utc))})\n\n"
         f"Ключ:\n<code>{get_order_perm_key(order.id)}</code>"
     )
