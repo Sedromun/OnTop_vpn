c3c385bdc0be623cedbf74dea8eb10355e00d335
sedromun <vanart2004@mail.ru>
2024-12-04 15:29:01 +0300

    fix

--- servers/vless_keys.py
+++ servers/vless_keys.py
@@ -36,8 +36,9 @@ async def get_vless_keys(order_id: int) -> str:
                 expiry_time=str(int(order.expiration_date.timestamp()) * 1000)
             )
             await api.client.add(vless_inbound_id[id], [client])
-        else:
-            print(client.expiry_time)
+        elif client.expiry_time != str(int(order.expiration_date.timestamp()) * 1000):
+            client.expiry_time = str(int(order.expiration_date.timestamp()) * 1000)
+            await api.client.update(order.uuid, client)
 
         order = get_order(order_id)
         res += create_key_string_from_data(id, order.uuid, client) + '\n'
